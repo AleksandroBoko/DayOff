cbdfb79f2f03fafe62b6ca7e8979415256af4ada
Aleks B <aleksandroboko@gmail.com>
2018-01-30 15:18:29 +0200

    Add controller Info

---
+++ DayOff/Controllers/InfoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DayOff.Controllers
+{
+    [RoutePrefix("api/info")]
+    public class InfoController : ApiController
+    {
+        [Route("remotegames")]
+        public string GetRemoteGames()
+        {
+            var request = WebRequest.Create("http://localhost:49500//api/game");
+            request.Method = "GET";
+            using (var response = request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var streamReader = new StreamReader(stream))
+                    {
+                        var result = streamReader.ReadToEnd();
+                        response.Close();
+                        return result;
+                    }
+                }
+            }
+        }
+
+    }
+}
